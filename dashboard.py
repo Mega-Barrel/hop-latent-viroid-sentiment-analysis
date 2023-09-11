2782989fb199e0375e73fcc44ea59c605144c081
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-09-11 19:25:27 +0530

    Added Date filter

--- dashboard.py
+++ dashboard.py
@@ -9,23 +9,44 @@ from src.graphs import (
     polarity_bar as pb
 )
 
+from datetime import timedelta
 
+# Page Config
 st.set_page_config(layout="wide")
 
 st.title('Twitter Sentiment Analytics')
 
 DATA_PATH = 'data/data.csv'
 
-@st.cache_data
 def load_data():
     data = pd.read_csv(DATA_PATH, encoding='utf-8')
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data['timestamp'] = pd.to_datetime(data['timestamp'])
+    data['daily'] = pd.to_datetime(data['daily'], format='%Y-%m-%d').dt.date
     return data
 
 # load the data
 data = load_data()
+
+# Double ended date slider
+start_dt = data['daily'].min()
+end_dt = data['daily'].max()
+
+st.subheader('Date Filter')
+date_filter = st.slider(
+    "Select a date range",
+    min_value = start_dt,
+    max_value = end_dt,
+    value = (
+        start_dt, 
+        end_dt
+    ),
+    step = timedelta(days=1),
+    format="YYYY/MM/DD"
+)
+
+filtered_data = data[ ( data["daily"] >= date_filter[0] ) & ( data["daily"] <= date_filter[1] ) ]
 
 tweet_analysis, image_tweets, raw_data = st.tabs([
     'Tweet Analysis',
@@ -37,32 +58,32 @@ with tweet_analysis:
     # Metrics
     col1, col2, col3, col4, col5 = st.columns(5)
 
-    col1.metric('Total Tweets', value=len(data))
-    col2.metric('Total Contributors', value=len(data['username'].unique()))
-    col3.metric('Avg Sentiment Score', value=round(data.loc[:, 'tweet_polarity_score'].mean(), 2))
+    col1.metric('Total Tweets', value=len(filtered_data))
+    col2.metric('Total Contributors', value=len(filtered_data['username'].unique()))
+    col3.metric('Avg Sentiment Score', value=round(filtered_data.loc[:, 'tweet_polarity_score'].mean(), 2))
 
-    weekday = data.loc[data['is_weekday'] == 'weekday']
-    weekend = data.loc[data['is_weekday'] == 'weekend']
+    weekday = filtered_data.loc[filtered_data['is_weekday'] == 'weekday']
+    weekend = filtered_data.loc[filtered_data['is_weekday'] == 'weekend']
 
     col4.metric('Total Weekday Tweets', value=len(weekday))
     col5.metric('Total Weekend Tweets', value=len(weekend))
 
     # Daily tweets
-    st.plotly_chart(tt.tweet_trend_chart(data['daily']), use_container_width = True)
+    st.plotly_chart(tt.tweet_trend_chart(filtered_data['daily']), use_container_width = True)
 
     # Tweet Polarity Distribution and Weighted plot
     row1_col1, row1_col2 = st.columns(2)
-    row1_col1.plotly_chart(pp.polarity_pie_chart(data), use_container_width=True)
-    row1_col2.plotly_chart(wt.weighted_polarity_line_chart(data[['daily', 'tweet_polarity_score']]), use_container_width = True)
+    row1_col1.plotly_chart(pp.polarity_pie_chart(filtered_data), use_container_width=True)
+    row1_col2.plotly_chart(wt.weighted_polarity_line_chart(filtered_data[['daily', 'tweet_polarity_score']]), use_container_width = True)
 
     row2_col1, row2_col2 = st.columns(2)
-    row2_col1.plotly_chart(th.plot_hashtag(data['text']), use_container_width=True)
-    row2_col2.plotly_chart(pb.pos_neg_chart(data), use_container_width = True)
+    row2_col1.plotly_chart(th.plot_hashtag(filtered_data['text']), use_container_width=True)
+    row2_col2.plotly_chart(pb.pos_neg_chart(filtered_data), use_container_width = True)
 
 with image_tweets:
-    filtered_data = data[data['images'].notna()]
-    filtered_data = filtered_data[['timestamp', 'text', 'images', 'polarity_label', 'likes', 'retweets', 'quotes', 'replies']]
-    st.write(filtered_data)
+    image_filter = filtered_data[filtered_data['images'].notna()]
+    image_filter = image_filter[['timestamp', 'text', 'images', 'polarity_label', 'likes', 'retweets', 'quotes', 'replies']]
+    st.write(image_filter)
 
 with raw_data:
     st.subheader('Raw Data')
