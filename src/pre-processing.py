c59fffc6ae937d929ef236247745d4cfaf3448cd
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-09-27 19:08:23 +0530

    Updated code, to handle multiple data files

--- src/pre-processing.py
+++ src/pre-processing.py
@@ -9,18 +9,18 @@ analyzer = SentimentIntensityAnalyzer()
 
 def process_df(filepath, encoding):
     df = pd.read_csv(filepath, encoding=encoding)
-    df = df[['images', 'text', 'likes', 'replies', 'retweets', 'quotes', 'timestamp', 'url']]
+    df = df[['images', 'text', 'likes', 'replies', 'retweets', 'quotes', 'timestamp', 'url', 'verified', 'username']]
     return df
 
 def transform_data(df):
-    df['username'] = df['url'].apply(lambda x: x.split('/')[3])
-    df = df[['timestamp', 'username', 'text', 'url', 'images', 'likes', 'replies', 'retweets', 'quotes']]
+    df['user_name'] = df['username'].apply(lambda x: x[1:])
+    df = df[['timestamp', 'username', 'text', 'url', 'images', 'likes', 'replies', 'retweets', 'quotes', 'verified']]
     return df
 
 def weekday_weekends(x):
-    x = x[:10]
-    date = datetime.datetime.strptime(x, '%Y-%m-%d')
     try:
+        x = x[:10]
+        date = datetime.datetime.strptime(x, '%Y-%m-%d')
         day_name = calendar.day_name[date.weekday()]
         if day_name == 'Sunday' or day_name == 'Saturday':
             return 'weekend'
@@ -46,7 +46,7 @@ def get_daily(x):
 # Cleaning the text column
 def clean_tweet(text):
     # Removing all usernames
-    text = re.sub(r'@[A-Za-z0-9_]+', '', text)
+    text = re.sub(r'@[A-Za-z0-9_]+', '', str(text))
     # Removing all URLS/LINKS
     text = re.sub(r'https?:\/\/\S+', '', text)
     # Removing all NUMBERS AND PERCENTAGES
@@ -78,13 +78,22 @@ def getsentiment(score):
         return 'Neutral'
     
 def main():
-    file_1 = '../data/2020_01_2022_10_data.csv'
-    file_2 = '../data/2022_10_2023_09_data.csv'
+    file_1 = '../data/raw_data/cannabis_mould_2020_data.csv'
+    file_2 = '../data/raw_data/cannabis_mould_2021_data.csv'
+    file_3 = '../data/raw_data/cannabis_mould_2022_data.csv'
+    file_4 = '../data/raw_data/cannabis_mould_2023_data.csv'
 
-    df_1 = process_df(file_1, encoding='cp1252')
+    df_1 = process_df(file_1, encoding='utf-8')
+    print('Reading file 1 done')
     df_2 = process_df(file_2, encoding='utf-8')
-    
-    merged_df = pd.concat([df_1, df_2], ignore_index=True)
+    print('Reading file 2 done')
+    df_3 = process_df(file_3, encoding='utf-8')
+    print('Reading file 3 done')
+    df_4 = process_df(file_4, encoding='utf-8')
+    print('Reading file 4 done')
+
+    merged_df = pd.concat([df_1, df_2, df_3, df_4], ignore_index=True)
+    # merged_df.to_csv('test.csv', index=False)
     merged_df = transform_data(merged_df)
     
     merged_df['daily'] = merged_df['timestamp'].apply(get_daily)
@@ -95,7 +104,8 @@ def main():
     merged_df['tweet_polarity_score'] = merged_df['cleaned_tweet'].apply(get_polarity_score)
     merged_df['polarity_label'] = merged_df['tweet_polarity_score'].apply(getsentiment)
 
-    merged_df.to_csv('../data/data.csv', index=False, encoding='utf-8')
+    print(len(merged_df))
+    merged_df.to_csv('../data/cannabis_mould_data.csv', index=False, encoding='utf-8')
 
 if __name__ == '__main__':
     main()
